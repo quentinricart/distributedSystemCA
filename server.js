b770c35df8c9f3d0a8f80892f0200e62537003ac
x23176971 <142295833+quentinricart@users.noreply.github.com>
2024-04-20 15:58:30 +0100

    Adding first service in server.js

--- server.js
+++ server.js
@@ -11,7 +11,22 @@ const NoRobberyProto = grpc.loadPackageDefinition(packageDefinition).NoRobbery;
 // Creating gRPC server
 const server = new grpc.Server();
 
+const rooms = ['living room', 'bedroom', 'kitchen', 'bathroom'];
 
+// Adding services to the server
+server.addService(NoRobberyProto.NoRobbery.service, {
+    // Service method to set music timer
+    SetTimeMusic: (call, callback) => {
+        // Extracting desired time from the request
+        const desiredTime = call.request.time;
+        // Logic to set the time, e.g., interact with the security system
+        // For now, let's just return a confirmation message
+        const message = `Time set to ${desiredTime} minutes\n`;
+        // Sending response back to the client
+        callback(null, { message });
+        console.log(`The client has set the time to ${desiredTime}\n`);
+    },
+});
 
 // Binding and starting the server
 server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
